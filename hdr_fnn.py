1c55c5a6c7cbcaf8637a130050286a104352d4e4
Taarak Rapolu <63356126+Taarak9@users.noreply.github.com>
2021-05-21 20:12:22 +0530

    Add files via upload

---
+++ hdr_fnn.py
@@ -0,0 +1,6 @@
+from fnn import FNN
+from mnist_loader import load_data_wrapper 
+
+training_data, validation_data, test_data = load_data_wrapper()
+nn = FNN([784, 30 , 10])
+nn.SGD(training_data, 30, 10, 3.0, test_data=test_data)
