7cf5ffa3773bc170803db023307b116e82513ce3
Taarak Rapolu <63356126+Taarak9@users.noreply.github.com>
2021-05-21 20:19:36 +0530

    update nepochs

--- hdr_fnn.py
+++ hdr_fnn.py
@@ -3,4 +3,4 @@ from mnist_loader import load_data_wrapper
 
 training_data, validation_data, test_data = load_data_wrapper()
 nn = FNN([784, 30 , 10])
-nn.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+nn.SGD(training_data, 100, 10, 3.0, test_data=test_data)
