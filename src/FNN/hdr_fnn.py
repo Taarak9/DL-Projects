0fc003919770b5b46d40473aaf5560072bf48725
Taarak9 <taarak.rapolu@gmail.com>
2021-05-31 15:21:03 +0530

    using customneuralnet

--- src/FNN/hdr_fnn.py
+++ src/FNN/hdr_fnn.py
@@ -1,5 +1,7 @@
-from generalized_fnn import FNN
-from mnist_loader import load_data_wrapper
+import numpy as np
+from matplotlib import pyplot as plt
+from mnist_loader import load_data_wrapper 
+from customneuralnet import FNN
 
 # MNIST data split
 training_data, validation_data, test_data = load_data_wrapper()
@@ -12,7 +14,15 @@ hdr.add_layer(10, "sigmoid")
 
 # Trained for 30 epochs using Stochastic Gradient Descent (mini-batch size = 10 and learning_rate = 3)
 # Displaying Test results
-hdr.SGD(training_data, 30, 10, 3.0, test_data=test_data, task="classification")
+hdr.compile(training_data, 30, 10, 3.0, 0.1, "GD", "mini_batch", True, test_data, "classification")
 
-# epoch vs error graph
-hdr.logging(test_data)
+def display_image(input_vector) :
+    image = np.reshape(input_vector, (28, 28))
+
+    plt.imshow(image, cmap='gray')
+    plt.show()
+
+# test results for few samples
+for i in np.arange(0, 19):
+  display_image(test_data[i][0])
+  print("HDR output: ", np.argmax(hdr.feedforward(test_data[i][0])))
