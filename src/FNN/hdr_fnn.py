1b752c9be37538098b34e06030bd2886fd6e2c0f
Taarak9 <taarak.rapolu@gmail.com>
2021-06-10 23:10:20 +0530

    hdr with customdl 1.0.14

--- src/FNN/hdr_fnn.py
+++ src/FNN/hdr_fnn.py
@@ -12,9 +12,7 @@ hdr = FNN(784, "ce")
 hdr.add_layer(80, "sigmoid")
 hdr.add_layer(10, "sigmoid")
 
-# Trained for 30 epochs using Stochastic Gradient Descent (mini-batch size = 10 and learning_rate = 3)
-# Displaying Test results
-hdr.compile(training_data, 30, 10, 3.0, 0.1, "GD", "mini_batch", True, test_data, "classification")
+hdr.compile(training_data, test_data)
 
 def display_image(input_vector) :
     image = np.reshape(input_vector, (28, 28))
